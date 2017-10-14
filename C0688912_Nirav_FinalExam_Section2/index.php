c6d8c0042275a6474864204eb724d32ade0ca495
nirav4294 <32417560+nirav4294@users.noreply.github.com>
2017-10-14 14:43:14 -0400

    Add files via upload

---
+++ C0688912_Nirav_FinalExam_Section2/index.php
@@ -0,0 +1,21 @@
+
+<?php
+
+
+require_once(dirname(__FILE__) . "/common/HTMLview.php");
+require_once(dirname(__FILE__) . "/src/controller/LoginController.php");
+
+session_start();
+
+// Making the session contain info of what browser started the session, to prevent hijacking of session.
+if(!isset($_SESSION["httpAgent"]))
+{
+    $_SESSION["httpAgent"] = $_SERVER["HTTP_USER_AGENT"];
+}
+
+$view = new HTMLView();
+$ctrl = new \controller\LoginController();
+
+$htmlBody = $ctrl->checkActions();
+
+$view->echoHTML($htmlBody);
---
+++ C0688912_Nirav_FinalExam_Section2/src/view/LoginView.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace view;
+
+require_once("src/view/MessageView.php");
+
+class LoginView {
+    private $model;
+    private $messages;
+
+    public function __construct(\model\LoginModel $model)
+    {
+        $this->model = $model;
+        $this->messages = new \view\MessageView();
+    }
+
+    // Checks if the user has pressed the login button.
+    public function onClickLogin() {
+        if(isset($_POST["loginButton"]))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Checks if the user has pressed the logout button.
+    public function onClickLogout() {
+        if(isset($_GET['logout']))
+        {
+          return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Function for retrieving current time, date and year in Swedish.
+    public function getTime() {
+        setlocale(LC_ALL,"sv_SE.UTF8");
+        return strftime("%A, den %d %B år %Y. Klockan är [%X].");
+    }
+
+    public function sessionCheck() {
+        if ($_SESSION["httpAgent"] != $_SERVER["HTTP_USER_AGENT"])
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // Renders the page according to the user being logged in or not.
+    public function showPage() {
+
+        if($this->model->getLoginStatus() === false || $this->sessionCheck() === false)
+        {
+            $username = isset($_POST["username"]) ? $_POST["username"] : "";
+            return "
+            <h1>Login</h1>
+            <h3>login first</h3>
+            <form action='?login' method='post' name='loginForm'>
+                <fieldset>
+                    <legend>Login - please enter username and password </legend><p>"
+                    . $this->messages->load() . "</p>
+                    <label><strong>Username: </strong></label>
+                    <input type='text' name='username' value='$username' />
+                    <label><strong>Password: </strong></label>
+                    <input type='password' name='password' value='' /><br><br>
+                    <label><strong>want to remain logged in: </strong></label>
+                    <input type='checkbox' name='stayLoggedIn' /><br><br>
+                    <input type='submit' value='sign in' name='loginButton' />
+                 </fieldset>
+            </form>
+
+            <p>" . $this->getTime() . "</p>";
+        }
+        else
+        {
+            return "<h1>Hi:)</h1>
+                    <h3>" . $this->model->retriveUsername() . "  logged in</h3>
+                    <p>" . $this->messages->load() . "</p>
+                    <a href='?logout'>Log out</a>
+                    <p>" . $this->getTime() . "</p>";
+        }
+    }
+}
